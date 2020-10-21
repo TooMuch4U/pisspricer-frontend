4f96736f9e91a7f3817d56092f7c982f1b2d5668
Tom Rizzi <tcr59@uclive.ac.nz>
2020-10-22 11:18:35 +1300

    Implemented images on items page

--- src/components/Items.vue
+++ src/components/Items.vue
@@ -3,13 +3,15 @@
       <p>Items</p>
       <table v-if="item_list != null">
         <tr>
+          <th>Image</th>
           <th>Name</th>
-          <th>Brand</th>
+          <th>Store Count</th>
           <th>Best Price</th>
         </tr>
         <tr v-for="item in item_list" v-bind:key="item.sku">
+          <td><img :src="imageUrl(item.sku)"></td>
           <td>{{ item.name }}</td>
-          <td>{{ item.brand }}</td>
+          <td>{{ item.storeCount }}</td>
           <td>${{ item.bestPrice }}</td>
         </tr>
       </table>
@@ -20,7 +22,8 @@
 export default {
   data () {
     return {
-      item_list: null
+      item_list: null,
+      static_url: process.env.VUE_APP_STATIC_URL
     }
   },
   mounted: function () {
@@ -28,13 +31,16 @@ export default {
   },
   methods: {
     getItems: function () {
-      this.$http.get(process.env.API_URL + '/items', { params: { count: 24 } })
+      this.$http.get(process.env.API_URL + '/items', { params: { count: 24, search: 'flame' } })
         .then((res) => {
           this.item_list = res.data.items
         })
         .catch((res) => {
           console.log('Error ' + res)
         })
+    },
+    imageUrl: function (sku) {
+      return process.env.VUE_APP_STATIC_URL + sku + '.jpeg'
     }
   },
   name: 'Items'
