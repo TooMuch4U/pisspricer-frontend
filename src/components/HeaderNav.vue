a935932a26a5a24f708ad431752451c674247506
Tom Rizzi <tcr59@uclive.ac.nz>
2020-10-22 22:46:07 +1300

    Updated navbar arrangement

--- src/components/HeaderNav.vue
+++ src/components/HeaderNav.vue
@@ -1,37 +1,9 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light rounded">
-    <a class="navbar-brand" href="#">Pisspricer</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample09" aria-controls="navbarsExample09" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <div class="collapse navbar-collapse" id="navbarsExample09">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item">
-          <router-link
-            class="nav-link"
-            active-class="active"
-            exact
-            :to="{ name: 'home' }"
-          >
-            Home
-          </router-link>
-        </li>
-        <li class="nav-item">
-          <router-link
-            class="nav-link"
-            active-class="active"
-            exact
-            :to="{ name: 'items' }"
-          >
-            <i class="ion-compose"></i>Items
-          </router-link>
-        </li>
-      </ul>
-      <form class="form-inline my-2 my-md-0">
-        <input class="form-control" type="text" placeholder="Search" aria-label="Search">
-      </form>
-    </div>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light rounded text-center">
+    <router-link class="navbar-brand m-auto"
+                 :to="{ name: 'home' }">
+      Pisspricer
+    </router-link>
   </nav>
 </template>
 
@@ -40,3 +12,9 @@ export default {
   name: 'HeaderNav'
 }
 </script>
+
+<style>
+.navbar-brand {
+  font-size: 2rem;
+}
+</style>
