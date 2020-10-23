de7393777064d6b231cb73b9dd069d104e66cfb1
Tom Rizzi <tcr59@uclive.ac.nz>
2020-10-24 11:09:09 +1300

    Added bootstrap cols

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,11 @@
 <template>
   <div id="home" class="container">
-    <div></div>
+    <div class="row">
+      <div class="col-2"></div>
+      <div class="col-8">
+      </div>
+      <div class="col-2"></div>
+    </div>
   </div>
 </template>
 
