7dfd44c65b6fd6a387f2b93738b862a91485ee77
Tom Rizzi <tcr59@uclive.ac.nz>
2020-10-25 17:06:07 +1300

    Implemented item pagination

--- src/components/Items.vue
+++ src/components/Items.vue
@@ -2,21 +2,49 @@
     <div id="Items" class="container-fluid pt-3">
       <div class="row">
         <div class="col-1"/>
-        <div class="col-2 bg-dark"></div>
-        <div class="col-8">
-          <p class="text-left">
-            Showing {{ this.actualCount }} of {{ this.totalCount }}
-          </p>
+        <div class="col-3 bg-dark"></div>
+        <div class="col-7" v-if="isLoading">Loading...</div>
+        <div class="col-7" v-else>
+          <div class="container-fluid">
+            <div class="row">
+
+              <div class="col-6">
+                <p class="text-left">
+                  Showing {{ (this.currentPage - 1) * this.actualCount + 1 }} - {{ this.currentPage * this.actualCount }} of {{ this.totalCount }}
+                </p>
+              </div>
+
+              <div class="col-6">
+                <div class="float-right">
+                  <label for="sortby">Sort by: </label>
+                  <select id="sortby" v-model="order" class="float-right no-outline" @change="orderChange">
+                    <option value="best-match" selected="selected">Relevance</option>
+                    <option value="price-desc">Price high</option>
+                    <option value="price-asc">Price low</option>
+                    <option value="alpha-asc">Alphabetical</option>
+                  </select>
+                </div>
+              </div>
+
+            </div>
+          </div>
+
           <table class="text-left table" v-if="item_list != null">
             <tr v-for="item in item_list" v-bind:key="item.sku">
               <td class="text-center">
-                <img class="item-image" :src="imageUrl(item.sku)">
+                <img v-if="item.hasImage" class="item-image" :src="imageUrl(item.sku)">
+                <img v-else class="item-image" src="@/assets/logo.png">
               </td>
-              <td>{{ item.name }}</td>
-              <td>{{ item.storeCount }} stores</td>
+              <td>
+                <p class="mb-0">{{ item.name }}</p>
+                <p class="text-muted mb-0">In {{ item.storeCount }} store{{ item.storeCount == 1 ? '' : 's'}}</p></td>
+              <td></td>
               <td>${{ item.bestPrice }}</td>
             </tr>
           </table>
+
+          <Pagination :pages="pages" :currentPage.sync="currentPage" />
+
         </div>
         <div class="col-1"/>
       </div>
@@ -25,6 +53,8 @@
 
 <script>
 import {eventBus} from '@/main.js'
+import Pagination from '@/components/Pagination'
+
 export default {
   data () {
     return {
@@ -32,43 +62,60 @@ export default {
       static_url: process.env.VUE_APP_STATIC_URL,
       searchTerm: null,
       actualCount: 0,
-      totalCount: 0
+      totalCount: 0,
+      currentPage: 1,
+      isLoading: 1,
+      itemsPerPage: 24,
+      order: 'best-match'
     }
+  },
+  components: {
+    Pagination
   },
   mounted: function () {
     this.getItems()
   },
   created: function () {
-    /**
-     * Creates event listener to update items from other components
-     */
-    eventBus.$on('remoteUpdateItems', () => {
+    eventBus.$on('remoteUpdateItems', (page) => {
+      this.currentPage = page
       this.getItems()
     })
   },
+  computed: {
+    pages () {
+      if (this.isLoading || this.totalCount <= this.itemsPerPage) {
+        return [1]
+      }
+      return [
+        ...Array(Math.ceil(this.totalCount / this.itemsPerPage)).keys()
+      ].map(e => e + 1)
+    }
+  },
   methods: {
-    /**
-     * Gets items from api and updates item_list
-     */
     getItems: function () {
+      this.isLoading = 1
       this.searchTerm = this.$route.query.s
-      this.$http.get(process.env.API_URL + '/items', { params: { count: 24, search: this.searchTerm } })
+      this.$http.get(process.env.API_URL + '/items',
+        { params:
+            { count: this.itemsPerPage,
+              search: this.searchTerm,
+              index: ((this.currentPage - 1) * this.itemsPerPage),
+              order: this.order} })
         .then((res) => {
           this.item_list = res.data.items
           this.totalCount = res.data.totalCount
           this.actualCount = res.data.count
+          this.isLoading = 0
         })
         .catch((res) => {
           console.log('Error ' + res)
         })
     },
-    /**
-     * Returns the image url for a specified item based on sku
-     * @param sku Sku of item image wanted
-     * @returns {string} String of image url
-     */
     imageUrl: function (sku) {
       return process.env.VUE_APP_STATIC_URL + sku + '.jpeg'
+    },
+    orderChange: function () {
+      this.getItems()
     }
   },
   name: 'Items'
@@ -77,7 +124,11 @@ export default {
 
 <style scoped>
 .item-image {
-  max-width: 75px;
-  max-height: 75px;
+  max-width: 50px;
+  height: 50px;
+}
+.no-outline {
+  outline: none;
+  border: 0;
 }
 </style>
---
+++ src/components/Pagination.vue
@@ -0,0 +1,80 @@
+<template>
+  <nav>
+    <ul class="pagination">
+      <li
+        v-for="page in pages"
+        :data-test="`page-link-${page}`"
+        :key="page"
+        :class="paginationClass(page)"
+        @click.prevent="changePage(page)"
+      >
+        <a class="page-link" href v-text="page" />
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+import {eventBus} from '@/main.js'
+
+export default {
+  name: 'Pagination',
+  props: {
+    pages: {
+      type: Array,
+      required: true
+    },
+    currentPage: {
+      type: Number,
+      required: true
+    }
+  },
+  methods: {
+    changePage (goToPage) {
+      if (goToPage === this.currentPage) return
+      eventBus.$emit('remoteUpdateItems', goToPage)
+    },
+    paginationClass (page) {
+      return {
+        'page-item': true,
+        active: this.currentPage === page
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+.pagination {
+  display: inline;
+  width: 50px;
+}
+
+.page-item {
+  display: inline-block;
+}
+
+.page-link {
+  color: #c5c5c5;
+  border-color: #c5c5c5;
+}
+
+.page-item.active .page-link {
+  background-color: #c5c5c5;
+  border-color: #c5c5c5;
+}
+
+.page-item.active .page-link:hover {
+  color: black;
+  border-color: #c5c5c5;
+}
+
+.page-link:hover {
+  color: black;
+}
+
+.page-link:focus {
+  box-shadow: none;
+}
+</style>
