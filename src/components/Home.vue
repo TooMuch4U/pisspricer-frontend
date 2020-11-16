1eba860cc99930bf8a9cb347cb66024a2c99cc0b
Tom Rizzi <tcr59@uclive.ac.nz>
2020-11-16 22:20:31 +1300

    Added home page text

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,8 +1,9 @@
 <template>
   <div id="home" class="container">
-    <div class="row">
+    <div class="row main mt-3">
       <div class="col-2"></div>
       <div class="col-8">
+        <h4>Compare prices from New Zealand liquor stores, and get the most beer for your buck.</h4>
       </div>
       <div class="col-2"></div>
     </div>
@@ -16,5 +17,7 @@ export default {
 </script>
 
 <style scoped>
-
+.main {
+  height: 400px;
+}
 </style>
