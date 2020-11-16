554f0afc2bef0e92c6d6fcf05356a1abe41cb6ad
Tom Rizzi <tcr59@uclive.ac.nz>
2020-11-16 23:00:29 +1300

    Started implementing search suggestions

--- src/components/HeaderNav.vue
+++ src/components/HeaderNav.vue
@@ -8,9 +8,21 @@
           </router-link>
         </li>
         <li class="nav-item">
-            <input class="form-control mt-2 input-outline shadow-none" type="text" v-model="searchTerm" placeholder="Search" @keyup.enter="searchItems" aria-label="Search">
+            <input class="form-control mt-2 input-outline shadow-none"
+                   type="text"
+                   v-model="searchTerm"
+                   placeholder="Search"
+                   @keyup="getPreview"
+                   @keyup.enter="searchItems">
         </li>
       </ul>
+      <div class="mx-auto" id="suggestions">
+        <ul class="list-group">
+          <li class="list-group-item" v-for="item in items" :key="item.sku">
+            {{ item.name }}
+          </li>
+        </ul>
+      </div>
   </nav>
 </template>
 
@@ -19,7 +31,11 @@ import {eventBus} from '@/main.js'
 export default {
   data () {
     return {
-      searchTerm: null
+      searchTerm: null,
+      items: null,
+      itemsCount: 0,
+      itemsTotalCount: 0,
+      itemLimit: 5
     }
   },
   methods: {
@@ -30,6 +46,24 @@ export default {
       if (this.$route.name === 'items') {
         eventBus.$emit('updateCurrentPage', 1)
         eventBus.$emit('remoteUpdateItems')
+      }
+    },
+    getPreview () {
+      if (this.searchTerm.length > 1) {
+        // if current search term is longer than 1 char
+        this.$http.get(process.env.API_URL + '/items',
+          { params: {
+            search: this.searchTerm,
+            count: this.itemLimit
+          } })
+          .then((res) => {
+            this.items = res.data.items
+            this.itemsCount = res.data.count
+            this.itemsTotalCount = res.data.totalCount
+          })
+      } else {
+        // Don't get any items, search term not long enough
+        this.items = null
       }
     }
   },
@@ -52,5 +86,18 @@ export default {
 
 .border-bottom {
   border-bottom: black 1px;
+}
+
+#suggestions {
+  position: absolute;
+  border: 1px solid #d4d4d4;
+  border-bottom: none;
+  border-top: none;
+  z-index: 99;
+  /*position the autocomplete items to be the same width as the container:*/
+  top: 100%;
+  left: 35%;
+  right: 35%;
+  background-color: white;
 }
 </style>
