fccd95b7f673184400feb1ab1da0590bdbf577bf
Tom Rizzi <tcr59@uclive.ac.nz>
2020-12-12 12:51:57 +1300

    Updated production urls

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,6 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  API_URL: '"https://api.pisspricer.co.nz/api/v1"',
+  VUE_APP_STATIC_URL: '"http://images.pisspricer.co.nz/"'
 }
