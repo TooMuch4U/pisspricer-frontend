697125887ae5b64713d696442ce59196163e1c40
Tom Rizzi <tcr59@uclive.ac.nz>
2020-12-12 13:21:54 +1300

    Updated images url

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,5 +2,5 @@
 module.exports = {
   NODE_ENV: '"production"',
   API_URL: '"https://api.pisspricer.co.nz/api/v1"',
-  VUE_APP_STATIC_URL: '"http://images.pisspricer.co.nz/"'
+  VUE_APP_STATIC_URL: '"https://storage.googleapis.com/pisspricer-bucket-dev/"'
 }
