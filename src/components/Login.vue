b272eba8ef9045982eb66263fa2c5f49334b2c7f
Tom Rizzi <tcr59@uclive.ac.nz>
2020-12-27 23:19:58 +1300

    Created login component

---
+++ src/components/Login.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="login" class="container-fluid pt-3">
+
+    <div class="row mb-3 mx-1">
+      <div class="col-lg-4 col-md-3 col-sm-2 d-none d-sm-block"/>
+
+      <div class="col-lg-4 col-md-6 col-sm-8 col-12 border-bottom">
+        <h4> Login </h4>
+      </div>
+
+      <div class="col-lg-4 col-md-3 col-sm-2 d-none d-sm-block"/>
+    </div>
+
+    <div class="row px-1">
+      <div class="col-lg-4 col-md-3 col-sm-2 d-none d-sm-block"/>
+
+      <div class="col-lg-4 col-md-6 col-sm-8 col-12">
+
+        <form>
+          <div class="form-group text-left">
+            <label for="exampleInputEmail1">Email address</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+          </div>
+          <div class="form-group text-left">
+            <label for="exampleInputPassword1">Password</label>
+            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+          </div>
+
+          <div class="form-row">
+            <div class="form-group col-6">
+              <button type="button" class="btn btn-block btn-light">Register</button>
+            </div>
+            <div class="form-group col-6">
+              <button type="submit" class="btn btn-block btn-primary">Login</button>
+            </div>
+          </div>
+        </form>
+
+      </div>
+
+      <div class="col-lg-4 col-md-3 col-sm-2 d-none d-sm-block"/>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'login'
+  }
+</script>
+
+<style scoped>
+#login {
+  min-height: 400px;
+}
+</style>
