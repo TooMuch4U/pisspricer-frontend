80b4a6db883ba5c4676c5f643f88acd6b46d91a5
Tom Rizzi <tcr59@uclive.ac.nz>
2020-12-29 19:05:57 +1300

    Added location store

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import user from './modules/user'
+import location from './modules/location'
 
 Vue.use(Vuex)
 
@@ -8,7 +9,8 @@ const debug = process.env.NODE_ENV !== 'production'
 
 export default new Vuex.Store({
   modules: {
-    user
+    user,
+    location
   },
   strict: debug
 })
