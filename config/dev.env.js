75a0fa141e6c68a50bb62b96ffaf760e96ff0c1c
Tom Rizzi <tcr59@uclive.ac.nz>
2021-01-15 22:47:50 +1300

    Updated dev images url

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,5 +5,5 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   API_URL: '"http://localhost:4941/api/v1"',
-  VUE_APP_STATIC_URL: '"https://storage.googleapis.com/pisspricer-bucket-dev/"'
+  VUE_APP_STATIC_URL: '"https://storage.googleapis.com/images.pisspricer.co.nz/"'
 })
