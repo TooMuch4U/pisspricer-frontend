cfad4856d1c689b22be4248d21f7f8bc372ae328
Tom Rizzi <tcr59@uclive.ac.nz>
2021-01-20 17:24:56 +1300

    Rearranged page

--- src/components/AdminPage.vue
+++ src/components/AdminPage.vue
@@ -1,36 +1,46 @@
 <template>
   <div id="home" class="container">
-    <div class="row py-3 bottom-border">
-      <div class="col-sm-2 d-none d-sm-block"></div>
-      <div class="col-sm-8 col-12">
-        <h4>Admin</h4>
+    <div v-if="!isAdmin">
+      <div class="row py-3">
+        <div class="col-sm-2 d-none d-sm-block"></div>
+
+        <div class="col-sm-8 col-12 min-height">
+          <p v-if="loggedIn">
+            Oops! You don't have sufficient privileges to view this page. Perhaps you're using the wrong account? <router-link :to="{name: 'logout'}">Logout</router-link>
+          </p>
+          <p v-else>
+            You're not logged in. <router-link :to="{name: 'login'}">Login</router-link>
+          </p>
+        </div>
+
+        <div class="col-sm-2 d-none d-sm-block"></div>
       </div>
-      <div class="col-sm-2 d-none d-sm-block"></div>
     </div>
 
-    <div class="row py-3 mx-1">
-      <div class="col-sm-2 d-none d-sm-block"></div>
-      <div class="col-sm-8 col-12">
-        <h4 class="mb-0"></h4>
-      </div>
-      <div class="col-sm-2 d-none d-sm-block"></div>
-    </div>
-
-    <div class="row text-center">
-      <div class="col-12 text-center">
-        <p v-if="!loggedIn">
-          You're not logged in. <router-link :to="{name: 'login'}">Login</router-link>
-        </p>
-
-        <p v-else-if="!isAdmin">
-          You're not an admin. <router-link :to="{name: 'logout'}">Logout</router-link>
-        </p>
-
-        <p v-else>
-          Admin!
-        </p>
+    <div v-else>
+      <div class="row py-3 bottom-border">
+        <div class="col-sm-2 d-none d-sm-block"></div>
+        <div class="col-sm-8 col-12">
+          <h4>Admin</h4>
+        </div>
+        <div class="col-sm-2 d-none d-sm-block"></div>
       </div>
 
+      <div class="row py-3 mx-1">
+        <div class="col-sm-2 d-none d-sm-block"></div>
+        <div class="col-sm-8 col-12">
+          <h4 class="mb-0"></h4>
+        </div>
+        <div class="col-sm-2 d-none d-sm-block"></div>
+      </div>
+
+      <div class="row text-center">
+        <div class="col-12 text-center">
+
+          body
+        </div>
+
+      </div>
     </div>
 
   </div>
@@ -47,5 +57,7 @@ export default {
 </script>
 
 <style scoped>
-
+.min-height {
+  min-height: 400px;
+}
 </style>
