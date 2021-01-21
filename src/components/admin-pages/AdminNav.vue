d58e3e21ddd9dec3aa42490656f70bcf07ea0c16
Tom Rizzi <tcr59@uclive.ac.nz>
2021-01-21 21:58:36 +1300

    Changed expand size

--- src/components/admin-pages/AdminNav.vue
+++ src/components/admin-pages/AdminNav.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light rounded mb-2">
+  <nav class="navbar navbar-expand navbar-light bg-light rounded mb-2">
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample09" aria-controls="navbarsExample09" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
