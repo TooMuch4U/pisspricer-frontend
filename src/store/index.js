f7fd17d1f4611456aad6231961fb0248c6bad348
Tom Rizzi <tcr59@uclive.ac.nz>
2021-01-21 18:58:21 +1300

    Added items store

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import user from './modules/user'
 import location from './modules/location'
+import items from './modules/items'
 
 Vue.use(Vuex)
 
@@ -10,7 +11,8 @@ const debug = process.env.NODE_ENV !== 'production'
 export default new Vuex.Store({
   modules: {
     user,
-    location
+    location,
+    items
   },
   strict: debug
 })
