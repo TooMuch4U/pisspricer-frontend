76e0810bad4a591a71981b86b92c509a239b21f7
Tom Rizzi <tcr59@uclive.ac.nz>
2021-01-30 13:53:07 +1300

    Added wrench icon

--- src/fontAwesome.js
+++ src/fontAwesome.js
@@ -1,9 +1,9 @@
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faUser as faUserSolid } from '@fortawesome/free-solid-svg-icons'
+import { faUser as faUserSolid, faWrench } from '@fortawesome/free-solid-svg-icons'
 import { faUser as faUserReg } from '@fortawesome/free-regular-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import Vue from 'vue'
 
 // Font awesome icons
-library.add(faUserReg, faUserSolid)
+library.add(faUserReg, faUserSolid, faWrench)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
